f062db348b389ef0e58521cbedc436813e74d4b0
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-12 19:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-// returnFirstTwoDrivers() — Declare a variable with const that is assigned an 
-// anonymous function. The assigned function should accept an array of drivers as an argument and 
+// returnFirstTwoDrivers() — Declare a variable with const that is assigned an
+// anonymous function. The assigned function should accept an array of drivers as an argument and
 //return the first two drivers in the array.
 
-const returnFirstTwoDrivers =  function () {}; 
+const returnFirstTwoDrivers =  function () {
+  return drivers.name;
+}; 
