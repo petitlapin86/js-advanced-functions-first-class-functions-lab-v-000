5963c6df3d37881f22a7d69f58a31ced5d0b7053
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-12 19:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@
 const returnFirstTwoDrivers =  function (drivers) {
   return drivers.slice (0, 2);
 };
+
+//returnLastTwoDrivers() — Declare a variable with const that is assigned an anonymous function.
+// The assigned function should accept an array of drivers as an argument and return the 
+// last two drivers in the array.//
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice (2, 3);
+}
